78ca14cf384f8a125a67eee901f69b28a72a2b9c
Mary Raccoon <maryraccoon@yahoo.com>
2018-10-01 16:23:36 -0700

    pc commit

--- apps/Cars/models.py
+++ apps/Cars/models.py
@@ -8,37 +8,38 @@ NAME_REGEX = re.compile(r'^[a-zA-Z]+$')
 
 
 class UserManager(models.Manager):
-    def basic_validator(self, postData):
+    def basic_validator(self, postdata):
         errors = {}
-        if len(postData['name'])< 3:
+        if len(postdata['name']) < 3:
             errors['name'] = "Name should be at least 2 characters"
-        elif not 'name' in errors and not re.match(NAME_REGEX, postData['name']):
+        elif not 'name' in errors and not re.match(NAME_REGEX, postdata['name']):
             errors['name'] = "Name must only contain letters"
-        if not 'email' in errors and not re.match(EMAIL_REGEX, postData['email']):
+        if not 'email' in errors and not re.match(EMAIL_REGEX, postdata['email']):
             errors['email'] = "Email is invalid"
-        if len(postData['password'])< 8:
-            if len(postData['password'])< 1:
+        if len(postdata['password']) < 8:
+            if len(postdata['password']) < 1:
                 errors['password'] = "Password can't be blank"
             errors['password'] = "Password should be no fewer than 8 characters in length"
-        if postData['conf_password'] != postData['password']:
+        if postdata['conf_password'] != postdata['password']:
             errors['conf_password'] = "Passwords do not match"
         return errors
-        
-    def login_validator(self, postData):
+
+    def login_validator(self, postdata):
         errors = {}
-        if len(postData['email1']) < 1:
+        if len(postdata['email1']) < 1:
             errors['email1'] = "Not a valid email"
-        if len(User.objects.filter(email=postData['email1'])) == 0:
+        if len(User.objects.filter(email=postdata['email1'])) == 0:
             errors['email1'] = "Email does not exist. Register first!"
-        if len(User.objects.filter(email=postData['email1'])) == 1:
-            password_hash = User.objects.get(email=postData['email1']).password
-            if bcrypt.checkpw(postData['password1'].encode(), password_hash.encode()) == False:  
+        if len(User.objects.filter(email=postdata['email1'])) == 1:
+            password_hash = User.objects.get(email=postdata['email1']).password
+            if bcrypt.checkpw(postdata['password1'].encode(), password_hash.encode()) == False:
                 errors['password1'] = "Incorrect password. Try again!"
         return errors
 
+
 class User(models.Model):
     name = models.CharField(max_length=255, blank=False)
-    email = models.CharField(max_length=255, unique=True )
+    email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -47,6 +48,15 @@ class User(models.Model):
 
     def __repr__(self):
         return "<User_object: {} {}>".format(self.name, self.email)
+
+
+class CarManager(models.Manager):
+    def car_validator(self, postdata):
+        errors = {}
+        if len(postdata['make']) < 3:
+            errors['make'] = "Make should be at least 2 characters"
+        return errors
+
 
 class Car(models.Model):
     make = models.CharField(max_length=45)
--- apps/Cars/views.py
+++ apps/Cars/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, HttpResponse, redirect
-from django.http import JsonResponse
+# from django.http import JsonResponse
 from django.contrib import messages
 from .models import *
-from django.core import serializers
-import json
-from django.db.models import Q
+# from django.core import serializers
+# import json
+# from django.db.models import Q
+
 
 def login(request):
     errors = User.objects.login_validator(request.POST)
@@ -12,20 +13,21 @@ def login(request):
         for key, value in errors.items():
             messages.error(request, value, extra_tags="login")
         return redirect('/')
-    user =User.objects.get(email = request.POST['email1'])
-    if(bcrypt.checkpw(request.POST['password1'].encode(), user.password.encode())):
+    user = User.objects.get(email=request.POST['email1'])
+    if bcrypt.checkpw(request.POST['password1'].encode(), user.password.encode()):
         print("password match")
         # user = User.objects.get(email=request.POST['email1'])
-        request.session['id']=user.id
+        request.session['id'] = user.id
         request.session['name'] = user.name
         request.session['email'] = user.email
-        messages.success(request, "You successfully loged in")
+        messages.success(request, "You successfully logged in")
         return redirect('/main')
     else:
         print("wrong password")
         return redirect('/')
     print(user.first_name)
-   
+
+
 def register(request):
     errors = User.objects.basic_validator(request.POST)
     if len(errors):
@@ -36,77 +38,87 @@ def register(request):
         hash_password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
         print(hash_password)
         user = User.objects.create(
-            name=request.POST['name'],  
+            name=request.POST['name'],
             email=request.POST['email'],
-            password = hash_password
+            password=hash_password
         )
         request.session['name'] = user.name
         messages.success(request, "User successfully created")
-        
+
         return redirect('/main')
+
 
 def index(request):
     return render(request, 'cars/index.html')
 
+
 def search(request):
     return redirect('/main')
 
+
 def main(request):
+    cars = Car.objects.all()
+    print("make:", cars)
     return render(request, 'cars/main.html')
 
+
 def display_cars(request, methods=['POST']):
-    all_cars=[]
-      
-    if request.POST['make'] == 'AnyMake' :
-        all_cars=Car.objects.filter(model__icontains=request.POST['model'],
-                                    year__icontains= request.POST['year'])
+    all_cars = []
+
+    if request.POST['make'] == 'AnyMake':
+        all_cars = Car.objects.filter(model__icontains=request.POST['model'],
+                                      year__icontains=request.POST['year'])
 
     if request.POST['model'] == 'AnyModel':
-        all_cars=Car.objects.filter(year__icontains=request.POST['year'],
-                                    make__icontains=request.POST['make'])
+        all_cars = Car.objects.filter(year__icontains=request.POST['year'],
+                                      make__icontains=request.POST['make'])
 
-    if request.POST['year'] == 'AnyYear' :
-        all_cars=Car.objects.filter(make__icontains=request.POST['make'],
-                                    model__icontains=request.POST['model'])
+    if request.POST['year'] == 'AnyYear':
+        all_cars = Car.objects.filter(make__icontains=request.POST['make'],
+                                      model__icontains=request.POST['model'])
 
-    if request.POST['year'] == 'AnyYear' and  request.POST['make'] == 'AnyMake' :
-        all_cars=Car.objects.filter(model__icontains=request.POST['model'])  
+    if request.POST['year'] == 'AnyYear' and request.POST['make'] == 'AnyMake':
+        all_cars = Car.objects.filter(model__icontains=request.POST['model'])
 
     if request.POST['model'] == 'AnyModel' and request.POST['year'] == 'AnyYear':
-        all_cars=Car.objects.filter(make__icontains=request.POST['make']) 
+        all_cars = Car.objects.filter(make__icontains=request.POST['make'])
 
-    if request.POST['make'] == 'AnyMake' and request.POST['model'] == 'AnyModel' :
-        all_cars=Car.objects.filter(year__icontains= request.POST['year']) 
+    if request.POST['make'] == 'AnyMake' and request.POST['model'] == 'AnyModel':
+        all_cars = Car.objects.filter(year__icontains=request.POST['year'])
 
-    if request.POST['make'] == 'AnyMake' and request.POST['model'] == 'AnyModel' and request.POST['year'] == 'AnyYear' :
-        all_cars=Car.objects.all()
-    if request.POST['make'] != 'AnyMake' and request.POST['model'] != 'AnyModel' and request.POST['year'] != 'AnyYear' :
-        all_cars=Car.objects.filter(make__icontains=request.POST['make'],
-                                    model__icontains=request.POST['model'],
-                                    year__icontains= request.POST['year'])
-    context={
-        "all_cars": all_cars,
+    if request.POST['make'] == 'AnyMake' and request.POST['model'] == 'AnyModel' and request.POST['year'] == 'AnyYear':
+        all_cars = Car.objects.all()
+    if request.POST['make'] != 'AnyMake' and request.POST['model'] != 'AnyModel' and request.POST['year'] != 'AnyYear':
+        all_cars = Car.objects.filter(make__icontains=request.POST['make'],
+                                      model__icontains=request.POST['model'],
+                                      year__icontains=request.POST['year'])
+    context = {
+        "all_cars": all_cars
     }
 
     return render(request, 'cars/display_cars.html', context)
 
+
 def show(request, id):
     context = {
-		'car': Car.objects.get(id = id)
-	}
+        'car': Car.objects.get(id=id)
+    }
     return render(request, 'cars/show.html', context)
+
 
 def logout(request):
     request.session.flush()
     return redirect('/')
 
-def favorites(request,number):
+
+def favorites(request, number):
     this_user = User.objects.get(id=request.session['id'])
-    this_car = Car.objects.get(id = number)
+    this_car = Car.objects.get(id=number)
     print("this_car", this_car.id)
     print("this_user", this_user.id)
     this_user.liked_cars.add(this_car)
     return render(request, 'cars/display_cars.html')
+
 
 def remove_fav(request, id):
     context = {
@@ -114,9 +126,10 @@ def remove_fav(request, id):
         'user': User.objects.get(id=request.session['id'])
     }
     this_user = User.objects.get(id=request.session['id'])
-    this_car = Car.objects.get(id = id)
+    this_car = Car.objects.get(id=id)
     this_user.liked_cars.remove(this_car)
     return redirect('/my_fav')
+
 
 def my_fav(request):
     user = User.objects.get(id=request.session['id'])
@@ -127,8 +140,10 @@ def my_fav(request):
     print("request.session id: ", request.session['id'])
     return render(request, 'cars/my_fav.html', context)
 
+
 def new_car(request):
     return render(request, 'cars/new_car.html')
+
 
 def add_car(request):
     # errors = Job.objects.job_validator(request.POST)
@@ -138,32 +153,35 @@ def add_car(request):
     #     return redirect('/new_car')
     # else:
     car = Car.objects.create(
-    make=request.POST['make_car'],
-    model=request.POST['model'],
-    year=request.POST['year'],
-    price=request.POST['price'],
-    pic=request.POST['pic'],
-    desc=request.POST['car_desc'], 
-    video=request.POST['video'],
-    author_id = request.session['id']
+        make=request.POST['make_car'],
+        model=request.POST['model'],
+        year=request.POST['year'],
+        price=request.POST['price'],
+        pic=request.POST['pic'],
+        desc=request.POST['car_desc'],
+        video=request.POST['video'],
+        author_id=request.session['id']
     )
     messages.success(request, "Car successfully created")
     return redirect('/new_car')
+
 
 def destroy_car(request, id):
     Car.objects.get(id=id).delete()
     print('Delete ', id)
     return redirect('/main')
 
+
 def edit_car(request, id):
     context = {
-		'car': Car.objects.get(id = id)
-	}
+        'car': Car.objects.get(id=id)
+    }
     return render(request, 'cars/edit_car.html', context)
+
 
 def update_car(request, id):
     if request.method == "POST":
-        up_car = Car.objects.get(id = id) 
+        up_car = Car.objects.get(id=id)
         up_car.make = request.POST['make_car']
         up_car.model = request.POST['model']
         up_car.year = request.POST['year']
